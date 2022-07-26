eec1809224f780ab47d864d82c036511361b152c
Malloc <mallocator@candassociates.com>
2022-07-26 13:15:52 +0500

    $_$

--- aus.py
+++ aus.py
@@ -1,30 +1,28 @@
-import random 
+import random
+
 rule = "/example"
 options = {}
-playerbalance = 10
-def main(actions):
-    playername = input('ник: ')
-    playermoney = int(input('ставка: '))
-    global playername, playermoney
-    if playermoney < playerbalance:
-        print('ERORR BALANCE') #
-    else:
-            if random.randint(1, 6) == 1:
-                if random.randint(1, 6) != 1:
-                    playerbalance *= 1.5
-                    print(f'Победил: {playername}')
-                    print(playerbalance)
-                else:
-                    print('Ничья')
-            else:
-                if random.randint(1, 6) == 1:
-                    playerbalance -= playermoney
-                    print('Поражение')
-                    print(playerbalance)
-                else:
-                    print('Ничья')
-    
-            
-    return "<h1>Hello</h1>"
-main(None)
 
+
+def main(actions):
+    playerbalance = 10
+    playername = input("ник: ")
+    playermoney = int(input("ставка: "))
+    if playermoney < playerbalance:
+        print("ERORR BALANCE")  #
+    else:
+        rand = random.randint(1, 6)
+        if rand <= 3:
+            playerbalance *= 1.5
+            print(f"Победил: {playername}")
+            print(playerbalance)
+        elif rand <= 5:
+            playerbalance -= playermoney
+            print("Поражение")
+            print(playerbalance)
+        else:
+            print("Ничья")
+    return "<h1>Hello</h1>"
+
+
+main(None)
